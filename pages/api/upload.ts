30c38a6ef0f1a8503c9b2bb312e324eb70e2c2a5
kothru <1894683+kothru@users.noreply.github.com>
2022-09-25 23:25:41 +0900

    json upload test

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,23 +1,46 @@
 import type { NextApiHandler } from "next";
 import { prisma } from "../../lib/prisma";
+import * as z from "zod";
+import * as formidable from "formidable";
+import * as fs from 'fs'
+
+const requestBodySchema = z.object({
+  email: z.string(),
+  name: z.string(),
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 const handler: NextApiHandler = async (req, res) => {
-  try {
-    // const result = requestBodySchema.parse(req.body);
-    console.log(req.body)
-    // await prisma.post.create({
-    //   data: {
-    //     title: result.title,
-    //     content: result.content,
-    //     published: true,
-    //   },
-    // });
-    res.json({
-      ok: true,
-    });
-    return;
-  } catch (error) {
-    res.json({ ok: false, error });
-  }
+  const form = new formidable.IncomingForm();
+  form.parse(req, async function (err, _fields, files) {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const file = files.file as formidable.File;
+    const jsonText = fs.readFileSync(file.filepath, 'utf-8')
+
+    try {
+      const result = requestBodySchema.parse(JSON.parse(jsonText))
+      await prisma.user.create({
+        data: {
+          email: result.email,
+          name: result.name,
+        },
+      });
+    } catch (error) {
+      console.error(error)
+    }
+  })
+
+  res.json({
+    ok: true,
+  });
+  return;
 };
 export default handler;
